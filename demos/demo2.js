9e9760946909abe0f2b9f0ecb30b161e713f3d03
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-07-13 14:40:47 +0200

    Fix demo02 to work with crafty v0.6.2

--- demos/demo2.js
+++ demos/demo2.js
@@ -8,7 +8,7 @@
 $(document).ready(function() {
         
     // Init Crafty Engine
-    Crafty.init(832, 512, 60).canvas.init();
+    Crafty.init(832, 512).canvas.init();
     Crafty.background('#000');
     
     // Change this to "DOM" if rendering is slow
@@ -21,15 +21,15 @@ $(document).ready(function() {
             "assets/RunningDood_Moosader.png",
             ], 
         function() {
-        	// load gfx
-    	    Crafty.sprite(64, "assets/InsideTiles_Moosader.png", {
-			    wall: [1,1],
-			    floor1: [0,2],
-			    floor2: [1,2],
-		    });
+            // load gfx
+            Crafty.sprite(64, "assets/InsideTiles_Moosader.png", {
+                wall: [1,1],
+                floor1: [0,2],
+                floor2: [1,2],
+            });
             Crafty.sprite("assets/RunningDood_Moosader.png", {
-			    player: [0, 0, 80, 30],
-		    });
+                player: [0, 0, 80, 30],
+            });
             
             $('#loading').hide();
             Crafty.scene('game');
@@ -53,25 +53,26 @@ $(document).ready(function() {
             $('#fps').text('FPS: ' + fps.value);
         });
         
-    	var zbase = 2;
-    	// draw tile floor
-    	for(var i = 0; i < 13; i++) {
-    		for(var j = 0; j < 8; j++) {
-    			Crafty.e("2D, " + render + ", floor2")
-    			.attr({x: i * 64, y: j * 64, z: zbase});
-    		}
-    	}
-    	// character animation 
-    	Crafty.c('CharAnims', {
-    		CharAnims: function() {
-    	        // setup animations sequences
-    	        this.requires("SpriteAnimation, Grid")
-    	        .animate("walk", [ [0, 90], [0, 60], [0, 30], [0, 0] ]);
-    	        return this;
-    	    }
-    	});    	
-    	// create character
-    	Crafty.e("2D, " + render + ", player, CharAnims, Multiway, MouseFace, BoxOverlays")
+        var zbase = 2;
+        // draw tile floor
+        for(var i = 0; i < 13; i++) {
+            for(var j = 0; j < 8; j++) {
+                Crafty.e("2D, " + render + ", floor2")
+                .attr({x: i * 64, y: j * 64, z: zbase});
+            }
+        }
+        // character animation 
+        Crafty.c('CharAnims', {
+            CharAnims: function() {
+                var animSpeed = 200
+                // setup animations sequences
+                this.requires("SpriteAnimation, Grid")
+                .reel("walk", animSpeed, [ [0, 90], [0, 60], [0, 30], [0, 0] ]);
+                return this;
+            }
+        });     
+        // create character
+        Crafty.e("2D, " + render + ", player, CharAnims, Multiway, MouseFace, BoxOverlays")
         .attr({
             move: {left: false, right: false, up: false, down: false},
             x: 400, y: 256, z: zbase + 1,
@@ -81,47 +82,47 @@ $(document).ready(function() {
         .origin('center')  // rotate origin
         .MouseFace({x: 40, y: 15})
         .CharAnims()
-    	.bind("Moved", function(from) {
-    		this.moving = true;
-    	})
-    	.bind("EnterFrame", function() {
-    		// If moving, adjust the proper animation and facing
-    		if (this.moving) {
-            	if (!this.isPlaying('walk'))
-            		this.stop().animate('walk', 8, -1); 
-	            this.moving = false;
-    		} else {
-    			this.stop();
-    		} 
-    	})
-    	.multiway(2, {W: -90, S: 90, D: 0, A: 180})
-    	.bind("MouseMoved", function(e) {
-    		// adjust player sprite facing
-    		// we add +90, since initially sprite is facing PI/2
-    		this.curAngle = (e.grad) + 90;
+        .bind("Moved", function(from) {
+            this.moving = true;
+        })
+        .bind("EnterFrame", function() {
+            // If moving, adjust the proper animation and facing
+            if (this.moving) {
+                if (!this.isPlaying('walk'))
+                    this.animate('walk', 8, -1); 
+                this.moving = false;
+            } else {
+                this.pauseAnimation();
+            } 
+        })
+        .multiway(2, {W: -90, S: 90, D: 0, A: 180})
+        .bind("MouseMoved", function(e) {
+            // adjust player sprite facing
+            // we add +90, since initially sprite is facing PI/2
+            this.curAngle = (e.grad) + 90;
             this.rotation = this.curAngle;
-    	})
-    	.bind("MouseUp", function(data) {
-			if (data.mouseButton == Crafty.mouseButtons.LEFT) {
-				// shoot - create bullet
-				Crafty.e("2D, " + render + ", Color")
-				.attr({
-					x: this.x + 40, y: this.y + 15, z: zbase + 1,
-					w: 3, h: 3,
-					speed: 5,
-					angle: this._dirAngle + Math.PI 
-				})
-				.color("#FA5656")
-				.bind("EnterFrame", function(frame) {
-					this.x += Math.cos(this.angle) * this.speed;
-					this.y += Math.sin(this.angle) * this.speed;
-					
-					if (this.x > Crafty.viewport.width || this.x < 0) {
-						this.destroy();
-					}
-				});
-			}
-		});
+        })
+        .bind("MouseUp", function(data) {
+            if (data.mouseButton == Crafty.mouseButtons.LEFT) {
+                // shoot - create bullet
+                Crafty.e("2D, " + render + ", Color")
+                .attr({
+                    x: this.x + 40, y: this.y + 15, z: zbase + 1,
+                    w: 3, h: 3,
+                    speed: 5,
+                    angle: this._dirAngle + Math.PI 
+                })
+                .color("#FA5656")
+                .bind("EnterFrame", function(frame) {
+                    this.x += Math.cos(this.angle) * this.speed;
+                    this.y += Math.sin(this.angle) * this.speed;
+                    
+                    if (this.x > Crafty.viewport.width || this.x < 0) {
+                        this.destroy();
+                    }
+                });
+            }
+        });
     });
     // start
     Crafty.scene('load');
